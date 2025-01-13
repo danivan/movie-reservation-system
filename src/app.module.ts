c5ded191ff1b9dbed013fdea08a38e590cfa879d
Dan Sentillas <d.sentillas@arcanys.com>
2025-01-13 12:37:51 +0800

    use config service for db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      password: process.env.DB_PASSWORD,
-      username: process.env.DB_USERNAME,
-      entities: [],
-      database: process.env.DB_NAME,
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [UserModule],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UserModule,
     ConfigModule.forRoot({
