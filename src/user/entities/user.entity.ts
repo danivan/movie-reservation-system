aa16c2c8317f6503909895221360bc8ae77a94b1
Dan Sentillas <d.sentillas@arcanys.com>
2025-01-14 15:26:18 +0800

    user module CRUD

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  firstname: string;
+
+  @Column()
+  lastname: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: false })
+  isAdmin: boolean;
+}
